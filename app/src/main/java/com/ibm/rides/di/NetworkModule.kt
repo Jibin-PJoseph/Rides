78fc79d7f3d1b096816b5c1477bcae2234ad56c0
jibinjoseph <jibinjoseph.dev22@gmail.com>
2024-10-02 18:11:24 -0600

    add: NetworkChecker

--- app/src/main/java/com/ibm/rides/di/NetworkModule.kt
+++ app/src/main/java/com/ibm/rides/di/NetworkModule.kt
@@ -1,5 +1,8 @@
 package com.ibm.rides.di
 
+import android.content.Context
+import com.ibm.rides.data.DefaultNetworkChecker
+import com.ibm.rides.data.NetworkChecker
 import com.ibm.rides.data.datasource.remote.RandomVehicleApi
 import com.ibm.rides.data.datasource.remote.RemoteDataSource
 import com.ibm.rides.data.repository.VehicleRepositoryImpl
@@ -8,6 +11,7 @@ import com.ibm.rides.domain.repository.VehicleRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import retrofit2.Retrofit
@@ -52,5 +56,13 @@ object NetworkModule {
              remoteDataSource,
              responseDtoMapper,
              dispatcher)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNetworkChecker(
+        @ApplicationContext context: Context
+    ): NetworkChecker {
+        return DefaultNetworkChecker(context)
     }
 }
